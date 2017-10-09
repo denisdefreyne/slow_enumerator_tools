18aa7f30d81d9a8e5a46f5a1c78e3d92f379361e
Denis Defreyne <denis.defreyne@stoneship.org>
2017-10-09 08:16:05 +0200

    Fix code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'slow_enumerator_tools'
-
 require 'fuubar'
 
 require 'simplecov'
@@ -9,6 +7,8 @@ SimpleCov.start
 
 require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
+require 'slow_enumerator_tools'
 
 RSpec.configure do |c|
   c.fuubar_progress_bar_options = {
